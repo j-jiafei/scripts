141b866668f855d7491e30c34b7d5f6e867ec75f
Jeff Jia <jeffjia@outlook.com>
2014-02-25 21:20:10 -0800

    add mongodb installation script

---
+++ install-mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# The script is used to install latest version of mongodb on ubuntu 12.04.
+# Feb 25, 2014
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
