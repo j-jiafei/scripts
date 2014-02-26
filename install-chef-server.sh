3b4852a6814491218f7482403eb4dee3a5aae4f8
Jeff Jia <jeffjia@outlook.com>
2014-02-25 21:48:35 -0800

    add wget

--- install-chef-server.sh
+++ install-chef-server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Install chef 11.0.11 server on ubuntu 12.04.
 
+mkdir -p $HOME/downloads
+pushd $HOME/downloads
+wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.11-1.ubuntu.12.04_amd64.deb
 sudo dpkg -i chef-server_11.0.11-1.ubuntu.12.04_amd64.deb
 sudo chef-server-ctl reconfigure
+popd
